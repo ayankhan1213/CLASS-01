f9270c9ef527faec26ecdf884c57cc49867ca456
Ayan Khan <aw1800606@gmail.com>
2026-08-17 20:33:06 -0700

    Some Practice

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 const fs = require('fs');
 
-fs.writeFile("hey.txt" , "Han Ayan" , (err)=>{
-    if(err) console.log("Error");
-    else console.log("done");
+// fs.writeFile("hey.txt" , "Han Ayan" , (err)=>{
+//     if(err) console.log("Error");
+//     else console.log("done");
     
     
-})
+// })
+
+// fs.appendFile("hey.txt" , " Kesa hai" , (err)=>{
+//     if(err) console.log("Error");
+//     else console.log("done");
+    
+    
+// })
+
+// fs.rename("hey.txt" , "hello.txt" , (err)=>{
+//     if(err) console.log("Error");
+//     else console.log("done");
+// })
+
+
+// fs.unlink("hello.txt" , (err)=>{
+//     if(err) console.log("Error");
+//     else console.log("done");
+// })
+
+
+// fs.writeFile("app.js" , "Hello Brother" , (err)=>{
+//     if(err) console.log(err.message);
+//     else console.log("created");
+// })
+
+
